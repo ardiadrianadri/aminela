98259810c9589e772e84528994bbc6cb4f7c1ee0
Alejandro <alejandro.martin.alonso@everis.com>
2016-06-09 14:49:56 +0200

    Modulo alta primera subida

---
+++ app/alta/alta.srv.js
@@ -0,0 +1,29 @@
+function loginSrvFactory ($http,$q,config,ErrorManager){
+
+    function loginClass () {
+        this.doLogin = function (email, psswd) {
+            var defer = $q.defer();
+            var serviceConfig = config.backService.loginConf;
+            
+            serviceConfig.data={
+                email:email,
+                password:psswd
+            };
+
+            $http(serviceConfig).then(function (result) {
+                defer.resolve(result.data);
+            }, function (error) {
+
+                var service = new ErrorManager();
+                defer.reject(service.getCustomError(error));
+
+            });
+
+            return defer.promise;
+        }
+    }
+    
+    return loginClass;
+}
+
+module.exports=angular.module('login').factory('LoginSrv',['$http','$q','config','ErrorManager',loginSrvFactory]);
