0828f023197f84ba5cdb945bb7126b379a0dba16
Carlos Guzman <carlosgv87@gmail.com>
2016-06-09 14:28:49 +0200

    Controller mio

--- app/login/login.ctrl.js
+++ app/login/login.ctrl.js
@@ -1,7 +1,40 @@
-function LoginCtrl (){
+function LoginCtrl ($filter,LoginSrv,$state) {
     
-    this.email="";
-    this.password="";
+    //ZONA DE DICICIONARIO
+    var vm = this;
+    vm.email = "";
+    vm.password = "";
+    vm.errorMsg = null;
+    
+    //FUNCIONES AUXILIARES
+    
+    //EVENTOS
+    vm.loginAction = function () {
+        var disable = $filter('PassFilter')(vm.password) || $filter('EmailFilter')(vm.email);
+        var service = new LoginSrv();
+        if (!disable) {
+            service.doLogin(vm.email,vm.email).then(function (data) {
+                console.log('Login con exito');
+            }), function () {
+                vm.errorMsg = error.usuario.msg;
+            }
+        }
+        alert("Se ha pulsado el boton");
+    }
+
+    vm.clean = function () {
+        vm.email = "";
+        vm.password = "";
+    }
+
+    vm.alta = function () {
+        $state.go('alta');
+    }
+
+    function startup () {
+        
+    }
+    startup();
 }
 
-module.exports = angular.module('login').controller('LoginCtrl',LoginCtrl);
+module.exports = angular.module('login').controller('LoginCtrl',['$filter',LoginCtrl]);
--- app/login/login.test.js
+++ app/login/login.test.js
@@ -57,11 +57,11 @@ describe('Test del modulo login', function () {
         }));
 
         it('Test OK: Probemos que obtenemos los datos correctos si el login es un exito',function () {
-           var logingData ={
-               email:'pepe@email.com',
-               password:'12345'
-           };
-
+            var logingData ={
+                email:'pepe@email.com',
+                password:'12345'
+            };
+            
             var servConfig = config.backService.loginConf;
             var service = new LoginSrv();
                 
@@ -78,6 +78,221 @@ describe('Test del modulo login', function () {
             
             $httpBackend.flush();
         });
+
+        it('Test KO: Prueba del error 404',function () {
+            var logingData ={
+                email:'pepe@email.com',
+                password:'12345'
+            };
+            
+            
+            var servConfig = config.backService.loginConf;
+            var errorConfig = config.serviceError;
+            var service = new LoginSrv();
+
+            $httpBackend.expect(servConfig.method, servConfig.url, logingData).respond(function () {
+                return [404,{},{}];
+            });
+
+            service.doLogin(logingData.email, logingData.password).
+            then(function (result) {
+                expect(false).toBe(true);
+            }, function (error) {
+                expect(error.usuario.msg).toBe(errorConfig['404']);
+            });
+
+            $httpBackend.flush();
+        });
+
+        it('Test KO: Prueba del error 401',function () {
+            var logingData ={
+                email:'pepe@email.com',
+                password:'12345'
+            };
+
+
+            var servConfig = config.backService.loginConf;
+            var errorConfig = config.serviceError;
+            var service = new LoginSrv();
+
+            $httpBackend.expect(servConfig.method, servConfig.url, logingData).respond(function () {
+                return [401,{},{}];
+            });
+
+            service.doLogin(logingData.email, logingData.password).
+            then(function (result) {
+                expect(false).toBe(true);
+            }, function (error) {
+                expect(error.usuario.msg).toBe(errorConfig['401']);
+            });
+
+            $httpBackend.flush();
+        });
+
+        it('Test KO: Prueba del error 403',function () {
+            var logingData ={
+                email:'pepe@email.com',
+                password:'12345'
+            };
+
+
+            var servConfig = config.backService.loginConf;
+            var errorConfig = config.serviceError;
+            var service = new LoginSrv();
+
+            $httpBackend.expect(servConfig.method, servConfig.url, logingData).respond(function () {
+                return [403,{},{}];
+            });
+
+            service.doLogin(logingData.email, logingData.password).
+            then(function (result) {
+                expect(false).toBe(true);
+            }, function (error) {
+                expect(error.usuario.msg).toBe(errorConfig['403']);
+            });
+
+            $httpBackend.flush();
+        });
+
+        it('Test KO: Prueba del error 500',function () {
+            var logingData ={
+                email:'pepe@email.com',
+                password:'12345'
+            };
+
+
+            var servConfig = config.backService.loginConf;
+            var errorConfig = config.serviceError;
+            var service = new LoginSrv();
+
+            $httpBackend.expect(servConfig.method, servConfig.url, logingData).respond(function () {
+                return [500,{},{}];
+            });
+
+            service.doLogin(logingData.email, logingData.password).
+            then(function (result) {
+                expect(false).toBe(true);
+            }, function (error) {
+                expect(error.usuario.msg).toBe(errorConfig['500']);
+            });
+
+            $httpBackend.flush();
+        });
+
+        it('Test KO: Prueba del error 500',function () {
+            var logingData ={
+                email:'pepe@email.com',
+                password:'12345'
+            };
+
+
+            var servConfig = config.backService.loginConf;
+            var errorConfig = config.serviceError;
+            var service = new LoginSrv();
+
+            $httpBackend.expect(servConfig.method, servConfig.url, logingData).respond(function () {
+                return [500,{},{}];
+            });
+
+            service.doLogin(logingData.email, logingData.password).
+            then(function (result) {
+                expect(false).toBe(true);
+            }, function (error) {
+                expect(error.usuario.msg).toBe(errorConfig.default);
+            });
+
+            $httpBackend.flush();
+        });
     });
 
+    describe('Test del controlador de login', function () {
+        var controlador, $httpBackend, config;
+        beforeEach(inject(function ($controller, $filter, LoginSrv, $state, _$httpBackend_, _config_) {
+            controlador = function () {
+                $httpBackend = _$httpBackend_;
+                config = _config_;
+                return $controller('LoginCtrl',{
+                    '$filter':$filter,
+                    'LoginSrv':LoginSrv,
+                    '$state':$state
+                });
+            }
+        }));
+
+        it('Test OK: Comprobemos que nuestro controlador tiene declardas las variables y eventos', function () {
+            var ctrl = controlador();
+
+            expect(ctrl.email).toBeDefined();
+            expect(ctrl.password).toBeDefined();
+            expect(ctrl.errorMsg).toBeDefined();
+            expect(ctrl.loginAction).toBeDefined();
+            expect(ctrl.clean).toBeDefined();
+            expect(ctrl.alta).toBeDefined();
+        });
+
+        it('Test KO: Comprobemos si el mail y el password no son validos no se llama al servicio', function () {
+            var ctrl = controlador();
+
+            ctrl.email = "";
+            ctrl.password = "";
+
+            var loginData = {
+                email: ctrl.email,
+                password: ctrl.password
+            };
+
+            var configServ = config.backService.loginConf;
+
+            $httpBackend.expect(configServ.method, configServ.url, loginData).respond(function () {
+                return [200,{},{}];
+            });
+
+            try {
+                ctr.loginAction();
+                $httpBackend.flush();
+                expect(true).toBe(false);
+            }catch (e) {
+                expect(true).toBe(true);
+            }
+        });
+
+        it('Test KO: Actualizamos el mensaje de error', function () {
+            var ctrl = controlador();
+
+            ctrl.email = "pepe@mail.com";
+            ctrl.password = "123123";
+
+            var loginData = {
+                email: ctrl.email,
+                password: ctrl.password
+            };
+
+            var configServ = config.backService.loginConf;
+            var configError = config.serviceError;
+
+            $httpBackend.expect(configServ.method, configServ.url, loginData).respond(function () {
+                return [404,{},{}];
+            });
+
+            try {
+                ctr.loginAction();
+                $httpBackend.flush();
+                expect(ctrl.errorMsg).toBe(configError['404']);
+            }catch (e) {
+                expect(true).toBe(false);
+            }
+        });
+
+        it('Test OK: Probemos que el clean limpua el email y el password', function () {
+            var ctrl = controlador();
+
+            ctrl.email = "pepe@mail.com";
+            ctrl.password = "123123";
+            
+            ctrl.clean();
+            
+            expect(ctrl.email).toBe("");
+            expect(ctrl.password).toBe("");
+        });
+    });
 });
